d8c3fa96dc820b9d65c35b675ea116ed37286e28
Vipershroom <undertalefan812@gmail.com>
2022-07-01 18:48:22 -0400

    Fix condition not working properly

--- binary_search/iterative.py
+++ binary_search/iterative.py
@@ -3,7 +3,7 @@ def binary_search(num_list, target):
     last = len(num_list)
     iterCheck = 0
 
-    while first +1 != last:
+    while first +1 != last or first -1 >= last:
         half = (first + last) // 2
 
         if num_list[half] == target:
@@ -16,4 +16,4 @@ def binary_search(num_list, target):
     
 
 test_case = [1,2,4,8,25, 27, 29, 30]
-print(binary_search(test_case, 2))
+print(binary_search(test_case, 32))
